7e8f0167ebeb295721053c834699e52341b709f1
yujit2003 <yujityadav99@gmail.com>
2023-07-12 23:42:24 +0530

    final backend

--- backend/index.js
+++ backend/index.js
@@ -4,48 +4,49 @@ const cors = require("cors");
 
 
 exports.getQuotes = async (req, res,next) => {
+  (async () => {
   res.set('Access-Control-Allow-Origin','*')
-  const browser = await puppeteer.launch({
-    headless: false,
-    defaultViewport: null,
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage",
-      "--single-process",
+    const browser = await puppeteer.launch({
+      headless: false,
+      defaultViewport: null,
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--single-process",
+      ]
+    });
+    const page = await browser.newPage();
+    await page.goto("https://www.startupindia.gov.in/content/sih/en/startupgov/regulatory_updates.html", {
+      waitUntil: "domcontentloaded",
+    });
+    const keys = [
+      'S.No.',
+      'date',
+      'ministry',
+      'notification'
     ]
-  });
-  const page = await browser.newPage();
-  await page.goto("https://www.startupindia.gov.in/content/sih/en/startupgov/regulatory_updates.html", {
-    waitUntil: "domcontentloaded",
-  });
-
-  const keys = [
-    'S.No.',
-    'date',
-    'ministry',
-    'notification'
-  ]
-
-  const result = await page.$$eval('body > div.market-research.onload > div:nth-child(2) > div > div > div > div > div > div > div > div > table > tbody > tr', rows => {
-  
-    return Array.from(rows, row => {
-      const columns = row.querySelectorAll('td');
+    
+    const result = await page.$$eval('body > div.market-research.onload > div:nth-child(2) > div > div > div > div > div > div > div > div > table > tbody > tr', rows => {
+      
+      return Array.from(rows, row => {
+        const columns = row.querySelectorAll('td');
       return Array.from(columns, column => 
         column.innerText);
+      });
     });
-  });
-  await browser.close();
+  })
+    await browser.close();
     // console.log(result);
-
-      try { 
-        res.set('Access-Control-Allow-Origin','*')
-        return res.status(200).json({
-          "policy": result
-        })
-      } catch (error) {
-        console.log(error);
-
-      }
-  
-};
+    
+    try { 
+      res.set('Access-Control-Allow-Origin','*')
+      return res.status(200).json({
+        "policy": result
+      })
+    } catch (error) {
+      console.log(error);
+      
+    }
+    
+  };
