b1d18a4f3cead8fbc4e245337b739cc3c925988c
Yujit Yadav <97657345+yujit2003@users.noreply.github.com>
2024-01-10 00:03:30 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,7 @@
 const puppeteer = require("puppeteer");
-const express = require("express");
-const cors = require("cors");
 
-
-exports.getQuotes = async (req, res,next) => {
-  (async () => {
-  res.set('Access-Control-Allow-Origin','*')
+exports.getQuotes = async (req, res, next) => {
+  try {
     const browser = await puppeteer.launch({
       headless: false,
       defaultViewport: null,
@@ -16,37 +12,27 @@ exports.getQuotes = async (req, res,next) => {
         "--single-process",
       ]
     });
-  })
+
     const page = await browser.newPage();
     await page.goto("https://www.startupindia.gov.in/content/sih/en/startupgov/regulatory_updates.html", {
       waitUntil: "domcontentloaded",
     });
-    const keys = [
-      'S.No.',
-      'date',
-      'ministry',
-      'notification'
-    ]
-    
+
     const result = await page.$$eval('body > div.market-research.onload > div:nth-child(2) > div > div > div > div > div > div > div > div > table > tbody > tr', rows => {
-      
       return Array.from(rows, row => {
         const columns = row.querySelectorAll('td');
-      return Array.from(columns, column => 
-        column.innerText);
+        return Array.from(columns, column => column.innerText);
       });
     });
+
     await browser.close();
-    // console.log(result);
-    
-    try { 
-      res.set('Access-Control-Allow-Origin','*')
-      return res.status(200).json({
-        "policy": result
-      })
-    } catch (error) {
-      console.log(error);
-      
-    }
-    
-  };
+
+    res.set('Access-Control-Allow-Origin', '*');
+    return res.status(200).json({
+      "policy": result
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal Server Error");
+  }
+};
